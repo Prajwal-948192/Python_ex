f039692524bb0595bfc37c86856c4d17309a54b6
Prajwal-948192 <dsouza.prajwal95@gmail.com>
2026-02-18 11:57:45 +0530

    Number of stops to goa

---
+++ BLR_GOA_stops.py
@@ -0,0 +1,23 @@
+'''
+     get the starting and ending reading of odomter and fuel in the tank before starting the ride and calcualte  mileage
+    calcualte the number of stops one need to make while travelling from BLR to GOA with dist of 560KM
+'''
+
+# # to get the mileage we need to know the fuel in the car before starting ride,  we need to get the data from user
+# use this formual to get mileage, distance travelled devided by fuel
+#get the fuel tank capacity from user
+# calcualte the distance traveled per one full tank using formuala mileage multiplied by fuel tank capacity
+#to calculate stops use the formuma BLR to GOA distance which is 560km devided by distance traveled per one full tank
+
+ODO_meter1 = int(input( "Please enter the starting reading of car odometer" ))
+ODO_meter2 = int(input( "Please enter the ending reading of car odometer" ))
+Fuel = float(input( " what the amount of fuel before starting the ride ?" ))
+Fuel_tank_capacity = float(input( " what is the fuel tank capacity of your car (liters) ? "))
+BLR_GOA_DIST = 560 #km
+distance_covered = ODO_meter2 - ODO_meter1
+mileage = distance_covered / Fuel
+distance_per_tankcapacity = mileage * Fuel_tank_capacity
+Total_stops = int(BLR_GOA_DIST / distance_per_tankcapacity)
+print( "you have traveled " , distance_covered , "km" , " and your car mileage is " , mileage )
+print ( " you can cover the distance of" , distance_per_tankcapacity, "km" , "with" , Fuel_tank_capacity , "liters fuel full tank capacity " )
+print( " to travel from Blr to Goa with a distance of 560km, you need to make" , Total_stops , " stops to refuel the full tank " )
